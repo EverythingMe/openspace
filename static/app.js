1d3075aebb838d88a6701d338fd2b0d641c75db9
Ran Ben Aharon <ran@ranbena.com>
2015-04-23 19:57:04 +0300

    Fixed tags layout

--- static/app.js
+++ static/app.js
@@ -3,22 +3,40 @@ Handlebars.registerHelper('color', function(str) {
   return getMaterialColor(str);
 });
 
-$.ajax('data/projects.json').done(function(data) {
-  // normalize
-  data.forEach(function(project) {
-    project.tags = project.tags.map(function(tag) {
-      return {'name': tag};
-    });
-  });
+var App = function(_cfg) {
+  var cfg = {
+    dataPath: 'data/projects.json',
+    projectsSel: '#projects',
+    tagsSel: '#tag-filter'
+  };
+  for (var k in _cfg) {
+    cfg[k] = _cfg[k];
+  }
 
-  // tag filters
-  (function() {
-    var tags = [];
-    data.forEach(function(project) {
-      tags = tags.concat(project.tags)
+  var projectsEl = $(cfg.projectsSel);
+
+  $.ajax(cfg.dataPath)
+    .done(function(d) {
+      normalizeData(d);
+      initTags(d);
+      initProjects(d);
     });
-    tags = _.uniq(tags, function(item){
-      return JSON.stringify(item);
+
+  var normalizeData = function(data) {
+    data.forEach(function(p) {
+      p.tags = p.tags.map(function(tag) {
+        return {'name': tag};
+      });
+    });
+  };
+
+  var initTags = function(data) {
+    var tags = [];
+    data.forEach(function(p) {
+      tags = tags.concat(p.tags)
+    });
+    tags = _.uniq(tags, function(t){
+      return JSON.stringify(t);
     });
 
     var source   = $('#tag-filter-template').html();
@@ -26,15 +44,14 @@ $.ajax('data/projects.json').done(function(data) {
     var html = template({ 'tags': tags })
 
     $('#tag-filter').append(html)
-  })();
+  };
 
-  // projects
-  (function() {
+  var initProjects = function(data) {
     var source   = $('#project-template').html();
     var template = Handlebars.compile(source);
     var html = template({ 'projects': data })
 
-    $('#projects')
+    projectsEl
       .html(html)
       .mixItUp({
         animation: {
@@ -43,5 +60,25 @@ $.ajax('data/projects.json').done(function(data) {
           easing: 'ease'
         }
       });
-  })();
- });
+  };
+
+  var filter = function(name) {
+    var state = projectsEl.mixItUp('getState');
+    var currTag = $('button[data-tag="'+state.activeFilter+'"]');
+    var newTag = $('button[data-tag="'+name+'"]');
+    var activeClass = "active btn-success";
+
+    if (state.activeFilter == name) {
+      projectsEl.mixItUp('filter', 'all');
+      newTag.removeClass(activeClass);
+    } else {
+      projectsEl.mixItUp('filter', name);
+      currTag.removeClass(activeClass);
+      newTag.addClass(activeClass);
+    }
+  };
+
+  return {
+    filter: filter
+  };
+};
