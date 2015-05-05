e602436f8022429c7a5217351826f86f895c5562
Ran Ben Aharon <ran@ranbena.com>
2015-05-05 16:29:00 +0300

    Make panel tags clickable #8

--- static/app.js
+++ static/app.js
@@ -76,7 +76,7 @@ var App = function(_cfg) {
     })
   }
 
-  var filter = function(name) {
+  var filter = function(name, forceFilter) {
     if (projectsEl.mixItUp('isMixing')) return;
 
     var state = projectsEl.mixItUp('getState');
@@ -85,6 +85,7 @@ var App = function(_cfg) {
     var toggleClassName = "active";
 
     if (state.activeFilter == name) {
+      if (forceFilter) { return; }
       projectsEl.mixItUp('filter', 'all');
     } else {
       projectsEl.mixItUp('filter', name);
